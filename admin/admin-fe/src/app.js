68ab339f2617e12dc4e0cddbf1b7db0cc931a268
sunyang <1802220208@qq.com>
2019-06-17 21:41:32 +0800

    开发环境搭建，使用webpack引入AdminLTE

---
+++ admin/admin-fe/src/app.js
@@ -0,0 +1,6 @@
+const userImg = require('./assets/timg.jpg')
+let body=document.querySelector('body')
+let img=new Image()
+img.src=userImg
+body.appendChild(img)
+console.log('ok');
---
+++ admin/admin-fe/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    // 环境
+    mode: 'development',
+
+    // 入口文件
+    entry: './src/app.js',
+
+    // 出口文件
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, './dev')
+    },
+
+    // 配置server
+    devServer: {
+        contentBase: path.join(__dirname, './dev'),
+        compress: true,
+        port: 80
+    },
+
+    // 配置loader
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+
+    // 配置插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            // 目标文件名
+            filename: 'index.html',
+            // 源文件路径
+            template: './index.html'
+        }),
+        new CopyPlugin([
+            {
+                from: './src/public',
+                to: './public'
+            }
+        ])
+    ]
+}
